192d5e7976f19f0ffe194afe233b3b0e9f4d32ef
Henriette007 <henrietteschreiber007@gmail.com>
2025-05-08 21:45:36 +0200

    first commit

---
+++ src/main/java/com/hetta/anime_review_site/AnimeReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package com.hetta.anime_review_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeReviewSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeReviewSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hetta/anime_review_site/AnimeReviewSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hetta.anime_review_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeReviewSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
